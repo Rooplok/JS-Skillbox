3d474d105333cab199dd4a1c19dd4937a7ffcd96
Rooplok <nikitaka4urin@yandex.ru>
2023-11-15 21:55:57 +0500

    second word end

---
+++ math/index.js
@@ -0,0 +1,13 @@
+//координаты первой точки
+let x1 = 100;
+let y1 = 200;
+
+//координаты второй точки
+let x2 = 300;
+let y2 = 400;
+
+//Вычисление площади
+let length = Math.abs(x1 - x2);
+let width = Math.abs(y1 - y2);
+let square = length * width;
+console.log(square)
