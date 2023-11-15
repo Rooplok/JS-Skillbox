648ca5afd820b1b56bfd54af0f28715c5bb1e782
Rooplok <nikitaka4urin@yandex.ru>
2023-11-15 21:57:27 +0500

    1234

--- math/index.js
+++ math/index.js
@@ -1,13 +1,17 @@
+//Задача 1
 //координаты первой точки
-let x1 = 100;
-let y1 = 200;
+let x1 = 2;
+let y1 = 3;
 
 //координаты второй точки
-let x2 = 300;
-let y2 = 400;
+let x2 = 10;
+let y2 = 5;
 
 //Вычисление площади
 let length = Math.abs(x1 - x2);
 let width = Math.abs(y1 - y2);
 let square = length * width;
 console.log(square)
+
+//Задача 2
+//
