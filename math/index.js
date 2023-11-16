c3bc372fe404f6b0efa018d9dfa3b9ac7ea5ad5b
Rooplok <nikitaka4urin@yandex.ru>
2023-11-16 17:12:14 +0500

    2431

--- math/index.js
+++ math/index.js
@@ -12,6 +12,7 @@ let length = Math.abs(x1 - x2);
 let width = Math.abs(y1 - y2);
 let square = length * width;
 console.log('Площадь равна: ' + square)
+console.log();
 
 //Задача 2
 let a = 13.123456789;
@@ -29,5 +30,26 @@ console.log(a >= b)
 console.log(a <= b)
 console.log(a === b)
 console.log(a !== b)
+console.log();
 
 //Задача 3
+function f_random(n, m)
+{
+    let range = Math.abs(n - m);
+    let min = Math.min(n, m);
+    let ranNum = Math.round(Math.random() * range) + min;
+    return ranNum;
+}
+
+let ar = f_random(0, 100);
+let br = f_random(100, -5);
+
+console.log('ar = ' + ar)
+console.log('br = ' + br)
+
+console.log(ar > br)
+console.log(ar < br)
+console.log(ar >= br)
+console.log(ar <= br)
+console.log(ar === br);
+console.log(ar !== br);
