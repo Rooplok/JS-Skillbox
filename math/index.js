90a7d5eacf571347198476921f163987cd30b49b
Rooplok <nikitaka4urin@yandex.ru>
2023-11-16 16:44:26 +0500

    2354

--- math/index.js
+++ math/index.js
@@ -11,7 +11,23 @@ let y2 = 5;
 let length = Math.abs(x1 - x2);
 let width = Math.abs(y1 - y2);
 let square = length * width;
-console.log(square)
+console.log('Площадь равна: ' + square)
 
 //Задача 2
-//
+let a = 13.123456789;
+let b = 2.123;
+let n = 5;
+
+a = Math.floor((a - Math.floor(a)) * Math.pow(10, n))
+b = Math.floor((b - Math.floor(b)) * Math.pow(10, n))
+
+console.log('a = ' + a)
+console.log('b = ' + b)
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
+console.log(a === b)
+console.log(a !== b)
+
+//Задача 3
