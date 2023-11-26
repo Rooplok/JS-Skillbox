b413f2145062d4e4ae3b3ea99d5039dbdd459fa0
Rooplok <nikitaka4urin@yandex.ru>
2023-11-26 18:43:03 +0500

    01_lec

---
+++ Lectures/01/index.js
@@ -0,0 +1,92 @@
+const game = document.querySelector('.game');
+const res = document.querySelector('.res');
+const btnGame = document.querySelector('.new-game');
+const fields = document.querySelector('.field');
+let stepCross = true;
+let count = 0;
+
+const circle = `<svg class="circle"> 
+<circle r="45" cx="58" cy="58" 
+stroke="blue" stroke-width="10"
+fill="none"/>
+</svg>`;
+
+const cross = `<svg class="cross">
+<line class="first"
+x1="15" y1="15"
+x2="105" y2="105"
+stroke="red"
+stroke-width="10"
+stroke-linecap="round"/>
+
+<line class="second"
+x1="105" y1="15"
+x2="15" y2="105"
+stroke="red"
+stroke-width="10"
+stroke-linecap="round"/>
+</svg>`;
+
+game.addEventListener('click', init);
+btnGame.addEventListener('click', newGame)
+
+function doStep(target) {
+    target.innerHTML = stepCross ? cross : circle;
+    target.classList.add(stepCross ? 'x' : 'o');
+}
+
+function init(e) {
+    doStep(e.target);
+    stepCross = !stepCross;
+    count++;
+    win();
+}
+
+function newGame() {
+    res.innerHTML = '';
+    stepCross = true;
+    count = 0;
+    Array.from(fields).forEach(el => {
+        el.innerHTML = '';
+        el.classList.remove('x', 'o', 'active');
+    });
+    game.addEventListener('click', init);
+}
+
+function win() {
+    let result = '';
+
+    const comb = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < comb.length; i++) {
+        const cur = comb[i];
+        const curFields = [fields[cur[0]].classList, fields[cur[1]].classList, fields[cur[2]].classList]
+        
+        if (curFields.every(el => el.classList.contains('X'))) {
+            res.innerHTML = 'Выиграли X'
+            curFields.forEach(field => field.classList.add('active'));
+            game.removeEventListener('click', init);
+            break;
+        }
+        if (curFields.every(el => el.classList.contains('O'))) {
+            res.innerHTML = 'Выиграли O'
+            curFields.forEach(field => field.classList.add('active'));
+            game.removeEventListener('click', init);
+            break;
+        }
+        if (count == 9) {
+            res.innerHTML = "Ничья"
+            game.removeEventListener('click', init);
+            break;
+        }
+    }
+}
